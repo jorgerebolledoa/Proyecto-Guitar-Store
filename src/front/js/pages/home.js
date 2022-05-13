421fc50db1dfed742cd8ac23e64a011253038717
jorgerebolledoa <aziel.az18@gmail.com>
2022-05-13 20:44:24 +0000

    sin errores basico

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,14 +3,14 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import Cards from "./Cards.jsx";
-import Carousel from "./Carousel-home";
+import Carousel from "./Carousel-home.jsx";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
     <>
-      <Carousel img="https://picsum.photos/400/300" />
+      <Carousel img="https://picsum.photos/1080/720" />
       <Cards />
       <div className="text-center mt-5">
         <h1>Hello Rigo!!</h1>
