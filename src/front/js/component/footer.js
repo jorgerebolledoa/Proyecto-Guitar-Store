aa3d94f5dd30ca6e23ce40ae019d4d87722ad497
aziel <99465810+jorgerebolledoa@users.noreply.github.com>
2022-05-13 22:35:20 +0200

    Revert "navbar y footer"

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,37 +1,10 @@
 import React, { Component } from "react";
-import { GiGuitarHead } from "react-icons/gi";
-import { Link } from "react-router-dom";
 
-export const Footer = () => {
-
-	return (
-		<div className="footer expand-lg  bg-dark text-warning ">
-
-
-			<div className="container d-flex justify-content-center ">
-				<span className="nav-item text-warning">
-					< GiGuitarHead />
-				</span>
-
-				<span className="nav-item">
-					<Link className="nav-link active text-warning" to="agregar link" href="#">Customer Services</Link>
-				</span>
-				<span className="nav-item">
-					<Link className="nav-link active text-warning" to="agregar link" href="#">Comunnity FQA</Link>
-				</span>
-				<span className="nav-item dropdown">
-					<Link className="nav-link active text-warning" to="agregar link" href="#">Contact</Link>
-				</span>
-				<span className="nav-item">
-					<Link className="nav-link active text-warning" to="agregar link" href="#">Term of Use</Link>
-				</span>
-				<span className="nav-item">
-					<Link className="nav-link active text-warning" to="agregar link" href="#">Privacy Policy</Link>
-				</span>
-
-
-			</div>
-		</div>
-	)
-
-};
+export const Footer = () => (
+	<footer className="footer mt-auto py-3 text-center">
+		<p>
+			Made with <i className="fa fa-heart text-danger" /> by{" "}
+			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+		</p>
+	</footer>
+);
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,40 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {GiGuitarHead } from "react-icons/gi";
-import {FaShoppingCart} from "react-icons/fa"
-
-
-
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top ">
-			<div className="container d-flex">
-				<Link className="navbar-brand text-warning ml-5" to="/">
-				< GiGuitarHead/>
+		<nav className="navbar navbar-light bg-light">
+			<div className="container">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-						<li className="nav-item">
-							<Link className="nav-link active text-warning" to="algun lado" href="#">Marca 1</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link active text-warning" to="algun lado" href="#">Marca 2</Link>
-						</li>
-						<li className="nav-item dropdown">
-							<Link className="nav-link active text-warning" to="algun lado" href="#">Marca 3</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link active text-warning" to="algun lado" href="#">Marca 4</Link>
-						</li>
-					</ul>
-					<form className="d-flex me-5 ">
-						<button className="btn btn-outline text-warning ">
-							<FaShoppingCart/>
-						</button>
-					</form>
+				<div className="ml-auto">
+					<Link to="/demo">
+						<button className="btn btn-primary">Check the Context in action</button>
+					</Link>
 				</div>
 			</div>
 		</nav>
-	)
-}
+	);
+};
