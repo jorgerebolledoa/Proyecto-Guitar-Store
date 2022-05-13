cc1c2de6bb6cad3355ee341d90f26d97d005fa8c
jorgerebolledoa <aziel.az18@gmail.com>
2022-05-13 20:10:20 +0000

    cards

---
+++ src/front/js/pages/Cards.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Card from "./Card.jsx";
+
+const Cards = () => {
+  return (
+    <div className="container">
+      <div className="row mb-2">
+        <>
+          <div className=" col-12 ">
+            <Card
+              img="https://picsum.photos/400/300"
+              title="Card title"
+              text="Some quick example text to build on the card"
+              linkbtn="https://picsum.photos/400/300"
+              textbtn="Go somewhere"
+            />
+          </div>
+          <div className=" col-12 ">
+            <Card
+              img="https://picsum.photos/400/300"
+              title="Card title"
+              text="Some quick example text to build on the card"
+              linkbtn="https://picsum.photos/400/300"
+              textbtn="Go somewhere"
+            />
+          </div>
+          <div className=" col-12 ">
+            <Card
+              img="https://picsum.photos/400/300"
+              title="Card title"
+              text="Some quick example text to build on the card"
+              linkbtn="https://picsum.photos/400/300"
+              textbtn="Go somewhere"
+            />
+          </div>
+          <div className=" col-12 ">
+            <Card
+              img="https://picsum.photos/400/300"
+              title="Card title"
+              text="Some quick example text to build on the card"
+              linkbtn="https://picsum.photos/400/300"
+              textbtn="Go somewhere"
+            />
+          </div>
+        </>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
