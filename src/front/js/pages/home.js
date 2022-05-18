d005d5dd589d41c04044ff773b489e0e43f8f968
jorgerebolledoa <aziel.az18@gmail.com>
2022-05-18 13:48:01 +0000

    actualizacion

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,11 +1,9 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import Cards from "./Cards.jsx";
 import Login from "./forgot_password.js";
 import Carousel from "./Carousel-home.jsx";
-import { GiGuitarHead } from "react-icons/gi";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
@@ -14,23 +12,6 @@ export const Home = () => {
     <>
       <Carousel img="https://picsum.photos/1080/720" />
       <Cards />
-
-      <div className="text-center mt-5">
-        <h1>Hello Rigo!!</h1>
-        <p>
-          <img src={rigoImageUrl} />
-        </p>
-        <div className="alert alert-info">
-          {store.message ||
-            "Loading message from the backend (make sure your python backend is running)..."}
-        </div>
-        <p>
-          This boilerplate comes with lots of documentation:{" "}
-          <a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-            Read documentation
-          </a>
-        </p>
-      </div>
       <Login />
     </>
   );
