c5750662b677ab831d7aabe135c7b66fd4295cab
jorgerebolledoa <aziel.az18@gmail.com>
2022-05-23 19:34:20 +0000

    map page producto

--- src/front/js/pages/producto.js
+++ src/front/js/pages/producto.js
@@ -9,7 +9,7 @@ export const Producto = () => {
   const [lista, setlista] = useState([]);
 
   const urlApi =
-    "https://assets.breatheco.de/apis/fake/todos/user/jorgerebolledo";
+    "https://3001-jorgereboll-proyectofin-xn7vi0urb0g.ws-us45.gitpod.io/api/products";
 
   useEffect(() => {
     getTask(urlApi);
@@ -18,64 +18,25 @@ export const Producto = () => {
   const getTask = (url) => {
     fetch(url)
       .then((Response) => Response.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        console.log(data);
+        setlista(data);
+      })
       .catch((error) => console.log(error));
-  };
-
-  const updateTask = (url, task) => {
-    fetch(url, {
-      method: "PUT",
-      body: JSON.stringify(task),
-      headers: {
-        "content-type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error));
-  };
-
-  const crear = (e) => {
-    if (e.keyCode === 13 && e.target.value !== "") {
-      let newlist = [...lista, { label: e.target.value, done: true }];
-      setlista(newlist);
-      updateTask(urlApi, newlist);
-      e.target.value = "";
-    }
-  };
-  const deletelist = (index) => {
-    lista.splice(index, 1);
-    setlista([...lista]);
-    updateTask(urlApi, [...lista]);
   };
 
   return (
-    <>
-      <CarouselM img={X250} />
-
-      <div className="m-2 p-2">
+    lista.length > 0 &&
+    lista.map((tastk, index) => {
+      return (
         <Cardlateral
-          img={X250}
-          title="X250"
-          text="Some quick example text to build on the card"
+          img={tastk.img}
+          title={tastk.name}
+          text={tastk.description_1}
           linkbtn="/X250"
           textbtn="Comprar"
         />
-        <Cardlateral
-          img={X250}
-          title="X300"
-          text="Some quick example text to build on the card"
-          linkbtn="/X250"
-          textbtn="Comprar"
-        />
-        <Cardlateral
-          img={X250}
-          title="x400"
-          text="Some quick example text to build on the card"
-          linkbtn="https://picsum.photos/400/300"
-          textbtn="Comprar"
-        />
-      </div>
-    </>
+      );
+    })
   );
 };
