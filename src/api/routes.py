5a7d7b9b3030b0abf4368ea25055084a9efce990
benjalonso <alonsobenjaminlb@gmail.com>
2022-05-27 16:19:24 +0000

    ruta para filtrar products by catergory

--- src/api/routes.py
+++ src/api/routes.py
@@ -142,18 +142,6 @@ def get_products():
 def get_products_by_category_id(category_id):
     products = Product.query.filter_by(category_id)
     return jsonify(products.serialize()), 200
-    
-
-@api.route("/products/id_categori/<int:category_id>", methods=['GET'])
-def get_products_by_category_id(category_id):
-    products = Product.query.get(category_id)
-    return jsonify(products.serialize()), 200
-
-
-@api.route("/products/id_categori/<int:category_id>", methods=['GET'])
-def get_products_by_category_id(category_id):
-    products = Product.query.get(category_id)
-    return jsonify(products.serialize()), 200
 
 
 @api.route('/products', methods=['POST'])
