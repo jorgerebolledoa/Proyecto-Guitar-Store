e44e684ba545b6a0192fe27c8567f5c332fcfcc3
jorgerebolledoa <aziel.az18@gmail.com>
2022-05-30 12:05:54 -0400

    sin bugs y link img

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,8 +4,8 @@ import Mapeo_Slider from "../component/Vista_principal/Mapeo-Silder";
 export const Home = () => {
   return (
     <>
-      <Mapeo_Carousel />
+      {/* <Mapeo_Carousel /> */}
       <Mapeo_Slider />
     </>
   );
-};
+};  
