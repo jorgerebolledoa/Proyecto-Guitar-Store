7f292e8db3caa8b29cd6223853caaad6490a11b0
jorgerebolledoa <aziel.az18@gmail.com>
2022-06-06 18:12:07 -0400

    avaces contactanos page sin api

--- src/front/js/pages/Contacto.jsx
+++ src/front/js/pages/Contacto.jsx
@@ -1,16 +1,45 @@
 import React from "react";
 import "../../styles/contacto.css"
 export const Contacto = () => {
+  const urlApi =
+    "https://assets.breatheco.de/apis/fake/todos/user/jorgerebolledo";
+
+  const createTask = (url, task) => {
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(task),
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  };
+
+  const crear = (e) => {
+    if (e.keyCode === 13 && e.target.value !== "") {
+      let newlist = [...lista, { label: e.target.value, done: true }];
+      setlista(newlist);
+      createTask(urlApi, newlist);
+      e.target.value = "";
+    }
+  };
 
   return (
-    <div className="container-fluid bg-dark text-warning justify-content-center  contacto_container">
-      <div className=" m-3 p-3 ">
-        <label for="exampleFormControlInput1" className="form-label">Email address</label>
-        <input type="email" className="form-control bg-secondary border-0" id="exampleFormControlInput1" />
-      </div>
-      <div className=" m-3 p-3">
-        <label for="exampleFormControlTextarea1" className="form-label text-warning ">Example textarea</label>
-        <textarea className="form-control bg-secondary border-0 text-warning" id="exampleFormControlTextarea1" rows="10" />
+    <div>
+      <div className="container-fluid bg-dark text-warning justify-content-center  contacto_container border  rounded  border-light">
+        <div className=" m-3 p-3 ">
+          <label for="exampleFormControlInput1" className="form-label">Email</label>
+          <input type="email" className="form-control background_input  border-0" id="exampleFormControlInput1" />
+        </div>
+        <div className=" m-3 p-3">
+          <label for="exampleFormControlTextarea1" className="form-label text-warning ">Menssege</label>
+          <textarea className="form-control background_input border-0 text-warning" id="exampleFormControlTextarea1" rows="10" onClick={crear} />
+          <div className="float-end  p-2">
+            <button type="button" class="btn btn-dark text-warning" onClick={crear}>enviar</button>
+          </div>
+        </div>
       </div>
     </div>
   );
