998a983a9d0f9e0ed04c5e7269ef6a639a526c3e
jorgerebolledoa <aziel.az18@gmail.com>
2022-06-07 17:08:42 -0400

    cambio de letra

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -23,91 +23,89 @@ export const Navbar = () => {
   }, []);
 
   return (
-    <div className="container-fluid m-0 p-0 ">
-      <nav
-        className={`navbar mb-5 navbar-expand-lg  bg-${state.bg} fixed-top ${!!state.shadow ? state.shadow : ""
-          }`}
-      >
-        <div className="container d-flex">
-          <Link className="navbar-brand text-warning ml-5" to="/">
-            <GiGuitarHead />
+    <nav
+      className={`navbar mb-5 navbar-expand-lg  bg-${state.bg} fixed-top ${!!state.shadow ? state.shadow : ""
+        }`}
+    >
+      <div className="container d-flex">
+        <Link className="navbar-brand text-warning ml-5" to="/">
+          <GiGuitarHead />
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse  " id="navbarNavDropdown">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a
+                className="nav-link active text-warning"
+                href="/categoria/1"
+              >
+                Cort
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link active text-warning"
+                href="/categoria/2"
+              >
+                Strandbergguitars
+              </a>
+            </li>
+            <li className="nav-item ">
+              <a
+                className="nav-link active text-warning"
+                href="/categoria/3">
+                Fender
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link active text-warning"
+                href="/categoria/4">
+                prsguitars
+              </a>
+            </li>
+            <li className="nav-item">
+
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link active text-warning"
+                href="/categoria/4">
+                prsguitars
+              </a>
+            </li>
+          </ul>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active text-warning" to="/register">Registrate</Link>
+            </li>
+
+            <li>
+              {store.currentUser === null ? (
+                <Link to="/login" className="nav-link active text-warning" href="/login">
+                  Ingresa
+                </Link>
+              ) : (
+                <Link className="nav-link active text-warning" to="/" onClick={() => actions.getlogout()}>
+                  Cerrar sesion
+                </Link>
+              )}
+            </li>
+
+          </ul>
+          <form className="d-flex me-5 " />
+          <Link
+            className="btn btn-outline text-warning"
+            to="/carroDeCompras"
+            href="/carroDeCompras"
+          >
+            <FaShoppingCart />
           </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse  " id="navbarNavDropdown">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a
-                  className="nav-link active text-warning"
-                  href="/categoria/1"
-                >
-                  Cort
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link active text-warning"
-                  href="/categoria/2"
-                >
-                  Strandbergguitars
-                </a>
-              </li>
-              <li className="nav-item ">
-                <a
-                  className="nav-link active text-warning"
-                  href="/categoria/3">
-                  Fender
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link active text-warning"
-                  href="/categoria/4">
-                  prsguitars
-                </a>
-              </li>
-              <li className="nav-item">
-
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link active text-warning"
-                  href="/categoria/4">
-                  prsguitars
-                </a>
-              </li>
-            </ul>
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link active text-warning" to="/register">Registrate</Link>
-              </li>
-
-              <li>
-                {store.currentUser === null ? (
-                  <Link to="/login" className="nav-link active text-warning" href="/login">
-                    Ingresa
-                  </Link>
-                ) : (
-                  <Link className="nav-link active text-warning" to="/" onClick={() => actions.getlogout()}>
-                    Cerrar sesion
-                  </Link>
-                )}
-              </li>
-
-            </ul>
-            <form className="d-flex me-5 " />
-            <Link
-              className="btn btn-outline text-warning"
-              to="/carroDeCompras"
-              href="/carroDeCompras"
-            >
-              <FaShoppingCart />
-            </Link>
-            <form />
-          </div>
+          <form />
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
